95d910fde4bc6565044f0935d80eafd43ab97700
deijahmonay <deijah@shack15.com>
2024-08-14 22:56:04 -0700

    remove commented out code + change win/lose message

--- js/app.js
+++ js/app.js
@@ -1,4 +1,3 @@
-// console.log('sanity check!')
 /*---------------------------------Constants-----------------------------------*/
 //1) Define constants
 //Define halloween-themed words and max guesses allowed
@@ -18,7 +17,7 @@ let amountCorrect = 0 // variable to represent number of correct letters guessed
 
 /*----------------------------Cached Element Referneces---------------------------*/
 
-// const resultDisplayElement= document.querySelector('#result-display')
+
 const startButtonElement = document.querySelector('#start-button') // start game button
 const wordDisplayElement = document.querySelector('#word-display') // element that displays word to user/ player
 console.log({ startButtonElement, wordDisplayElement })
@@ -82,13 +81,12 @@ function gameMessage() {
     let message = document.querySelector('h1') // changes the hangman title(h1) to show win/loss message once game over
     message.innerHTML = '' // sets blank game message
     if (guessesLeft <= 0) {
-        message.innerHTML = `FAIL! The correct word was ${currentWord}` // render lose message to user
+        message.innerHTML = `The shawdows whisper your name... but it's too late. The word was ${currentWord}. The darkness now claims you!` // render lose message to user
     }
     else if (amountCorrect == currentWord.length) {
-        message.innerHTML = `CONGRATS! You guessed correct ${currentWord}` // render win message to user
+        message.innerHTML = `Darkness fades away.. ${currentWord} was your weapon!` // render win message to user
     }
-    // console.log('amountCorrect: '+ amountCorrect)
-    // console.log(amountCorrect, "<-- this is the amountCorrect")
+    
 }
 getKeyboard()
 //handle generating random selections for the word to guess
@@ -103,37 +101,3 @@ getKeyboard()
 document.querySelector('#start-button').addEventListener('click', startGame) //giving buttons a callback function
 
 
-
-
-// Game name: Haunted Hangman /or Ghostly Guessing (not decided yet)
-
-// As a user, I want to see a landing page when I arrive at the website, so I know I’m in the right place.
-// As a user, I want to see game instructions pop up --> undecided where
-// As a user, I want to see a start button 
-// As a user, I want to see a halloween themed game 
-// As a user, I want to see the _ marks that represent the number of letters in the word I need to guess
-// As a user, I want to be able to type a letter to guess  the word
-// As a user, I want tot be able to see that I get 3-4(not decided yet) attempts to guess letters in the word
-// As a user, I want to see a message appear if I run out of guesses
-// As a user, I want to see a message appear if I win with a spooky congratulations message
-// As a user, I want to see a button to restart the game after I win or lose
-// 1) Define any variables used to track the state of the game:
-
-// Halloween themed words to guess (mummy, skeleton, vampire, ghost, headless horseman, ghost) – might change the words to be more cohesive
-// Number of guesses for the user
-// Win/Lose messages for the user
-// 2) Define the required constants:
-
-// Halloween theme words (mummy, skeleton, vampire, ghost, headless horseman, ghost)
-// 3) Handle a user guessing a letter for a word:
-
-// Get the letter the user guesses 
-// Check if letter is in the word 
-// Win/Lose messages for the user
-// Track number of guesses left for user in the level
-// 5) Display message to the user playing the game:
-
-// Show a “COMPLETED!” Message if user guesses the word correctly (passes level)
-// Show a “FAILED :(” message if the user loses — show correct word (possibly — undecided)
-// Show a “RESTART(or)RETRY” message if failed & “Next Level” button if they guess correctly
-// ROCK PAPER SCISSORS (pseudocode page) used for reference.
